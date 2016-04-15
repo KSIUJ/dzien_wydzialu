9067cc0fad3980301a7b38ced8045f6508b8cac6
HawkeApache <adrian.lato@onet.pl>
2016-04-15 15:26:42 +0200

    Add new Models
    Lecturer, Activity, Event, Group

--- dzien_wydzialu/home/models.py
+++ dzien_wydzialu/home/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class Room(models.Model):
     number = models.PositiveSmallIntegerField()
     projector = models.BooleanField()
@@ -9,3 +9,27 @@ class Room(models.Model):
     info = models.TextField(max_length=3000)
     seats = models.PositiveSmallIntegerField()
 
+
+class Lecturer(models.Model):
+    name = models.CharField(max_length=250)
+    surname = models.CharField(max_length=250)
+    dergee = models.CharField(max_length=250)
+
+
+class Activity(models.Model):
+    title = models.CharField(max_length=250)
+    is_lecture = models.BooleanField()
+    lecturer = models.ManyToManyField(Lecturer) #to check
+    room = models.ForeignKey(Room)
+    description = models.TextField(max_length=3000)
+
+
+class Group(models.Model):
+    pass
+
+
+class Event(models.Model):
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    activity = models.ForeignKey(Activity)
+    group = models.ForeignKey(Group)
