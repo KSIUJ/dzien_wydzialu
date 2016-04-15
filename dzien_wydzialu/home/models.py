131d01a18a9455c2d1062c32aa8a4a542b33687e
HawkeApache <adrian.lato@onet.pl>
2016-04-15 15:02:45 +0200

    Add model Room

--- dzien_wydzialu/home/models.py
+++ dzien_wydzialu/home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    number = models.PositiveSmallIntegerField()
+    projector = models.BooleanField()
+    board = models.BooleanField()
+    computers = models.PositiveSmallIntegerField()
+    info = models.TextField(max_length=3000)
+    seats = models.PositiveSmallIntegerField()
+
