9522709ffad68f324351cee7f5d17ba9d3413791
sulek1995 <t.sulecki95@gmail.com>
2016-06-02 17:07:11 +0200

    New look of Home page,
    I changed css , also added new url

--- dzien_wydzialu/urls.py
+++ dzien_wydzialu/urls.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from dzien_wydzialu.home import views
 from registration.backends.default.views import RegistrationView
 from dzien_wydzialu.home.forms import ExRegistrationForm
+from dzien_wydzialu.home.forms import ExAuthenticationForm
+from django.views.generic import TemplateView
+
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -10,6 +14,8 @@ urlpatterns = [
     url(r'^program/$', views.program, name='program'),
     url(r'^program/group/(?P<group_id>\d+)', views.get_group_pdf, name='group_pdf'),
     url(r'^gallery/$', views.gallery, name='gallery'),
-    url(r'^accounts/register/$', RegistrationView.as_view(form_class = ExRegistrationForm), name = 'registration_register'),
+    url(r'^accounts/register/$', RegistrationView.as_view(form_class=ExRegistrationForm), name='registration_register'),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'authentication_form': ExAuthenticationForm},
+        name='auth_login'),
     url(r'^accounts/', include('registration.backends.default.urls')),
-]
+ ]
