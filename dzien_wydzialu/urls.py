669b01b565a8c2a6a5a7c232de4368a083cc8e16
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2016-06-02 17:34:57 +0200

    Revert "after conflict"
    This reverts commit 8b3a01c30a3ed156a71c0f261831857bcdcb2660, reversing
    changes made to 01ca2f446f82702b87c5c6f0a63cd20d1293c245.

--- dzien_wydzialu/urls.py
+++ dzien_wydzialu/urls.py
@@ -3,19 +3,36 @@ from django.contrib import admin
 from dzien_wydzialu.home import views
 from registration.backends.default.views import RegistrationView
 from dzien_wydzialu.home.forms import ExRegistrationForm
-from dzien_wydzialu.home.forms import ExAuthenticationForm
-from django.views.generic import TemplateView
 
-
+accounts_patterns = [
+    url(r'^register/$',
+        RegistrationView.as_view(form_class=ExRegistrationForm),
+        name='registration_register'),
+    url(r'^', include('registration.backends.default.urls')),
+]
 
 urlpatterns = [
+    url(r'^$',
+        views.index,
+        name='homepage'),
+    url(r'^gallery/$',
+        views.gallery,
+        name='gallery'),
+    url(r'^program/$',
+        views.program,
+        name='program'),
+    url(r'^program/group/(?P<group_id>\d+)',
+        views.get_group_pdf,
+        name='group_pdf'),
+    url(r'^visitorgroup/$',
+        views.visitorgroup_index,
+        name='visitorgroup_index'),
+    url(r'^visitorgroup/new/$',
+        views.visitorgroup_new,
+        name='visitorgroup_new'),
+    url(r'^visitorgroup/assign/$',
+        views.visitorgroup_assign,
+        name='visitorgroup_assign'),
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index, name='homepage'),
-    url(r'^program/$', views.program, name='program'),
-    url(r'^program/group/(?P<group_id>\d+)', views.get_group_pdf, name='group_pdf'),
-    url(r'^gallery/$', views.gallery, name='gallery'),
-    url(r'^accounts/register/$', RegistrationView.as_view(form_class=ExRegistrationForm), name='registration_register'),
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'authentication_form': ExAuthenticationForm},
-        name='auth_login'),
-    url(r'^accounts/', include('registration.backends.default.urls')),
- ]
+    url(r'^accounts/', include(accounts_patterns)),
+]
